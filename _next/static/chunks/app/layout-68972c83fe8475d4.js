1cbdac8cbceee4bed60019b1de0c1de21f4df641
fadi batraki <fadibatraki321@gmail.com>
2025-01-17 09:10:49 +0100

    Updates

---
+++ _next/static/chunks/app/layout-68972c83fe8475d4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2085:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,9541,23)),Promise.resolve().then(l.t.bind(l,9324,23))},9324:()=>{},9541:e=>{e.exports={style:{fontFamily:"'Poppins', 'Poppins Fallback'",fontStyle:"normal"},className:"__className_34280e"}}},e=>{var s=s=>e(e.s=s);e.O(0,[908,441,517,358],()=>s(2085)),_N_E=e.O()}]);
