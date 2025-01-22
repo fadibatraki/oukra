c636e603ccbece370f465c97a474b87dc2192dad
fadi batraki <fadibatraki321@gmail.com>
2025-01-22 18:25:17 +0100

    Normalize line endings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/nav/NavBar";
+import Footer from "./components/footer/footer";
 
 const poppins = Poppins ({subsets :["latin"] , weight :['400' , '700'] });
 
@@ -17,8 +19,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
+      <div className="flex flex-col min-h-screen">
+      <NavBar/>
+        <main className="flex-grow">
         {children}
-      </body>
+        </main>
+        <Footer/>
+      </div>
+        </body>
     </html>
   );
 }
